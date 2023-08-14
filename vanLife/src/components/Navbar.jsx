404fc0f48c29a09a87120ce9e7d1a3add50d136e
koketsobogopane <koketsobogopanenov24@gmail.com>
2023-08-14 10:46:37 +0200

    Updating mountain img directory

--- vanLife/src/components/Navbar.jsx
+++ vanLife/src/components/Navbar.jsx
@@ -3,7 +3,14 @@ import { Link } from "react-router-dom"
 const Navbar = () => {
     return (
       <nav className="navbar">
-        <h2>#VANLIFE</h2>
+        <h2><Link 
+                style={{
+                    textDecoration: 'none',
+                    color: 'black',
+                    }}
+                to={"/"}
+
+        >#VANLIFE</Link></h2>
         <ul >
           <li><Link to="/Vans">Vans</Link></li>
           <li><Link to="/about">About</Link></li>
